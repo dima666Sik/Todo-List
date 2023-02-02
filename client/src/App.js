6f56367fa121ee77069f3531811c26c560465beb
Lazyness <leniviy.demon.1@gmail.com>
2023-02-03 01:29:10 +0200

    create skeleton project.

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+const TodoList = () => {
+	return (
+		<div className="todo-list">
+			<ul>
+				<li>First task</li>
+				<li>Second tack</li>
+			</ul>
+		</div>
+	);
+};
+
+const AppHeader = () => {
+	return <h1>Todo List</h1>;
+};
+
+const SearchPanel = () => {
+	return <input placeholder="search" />;
+};
+
+export default function App() {
+	return (
+		<div>
+			<AppHeader />
+			<SearchPanel />
+			<TodoList />
+		</div>
+	);
+}
