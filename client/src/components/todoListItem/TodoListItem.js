6ecf0fbe87cf0eddb88fc8e254c3a11f4dfcd122
Lazyness <leniviy.demon.1@gmail.com>
2023-02-05 23:32:54 +0200

    update readme file

--- client/src/components/todoListItem/TodoListItem.js
+++ client/src/components/todoListItem/TodoListItem.js
@@ -4,6 +4,7 @@ import "./TodoListItem.css";
 export default class TodoListItem extends Component {
 	state = {
 		done: false,
+		important: this.props.important === false ? false : true,
 	};
 
 	onLabelClick = () => {
@@ -14,31 +15,40 @@ export default class TodoListItem extends Component {
 		}
 	};
 
+	onBtnMarkImportantClick = () => {
+		if (!this.state.important) {
+			this.setState({ important: true });
+		} else {
+			this.setState({ important: false });
+		}
+	};
+
 	render() {
-		const { label, important } = this.props;
+		const { label } = this.props;
 		let classNameSpanLabels = `todo-list-item d-flex`;
+
 		const { done } = this.state;
+		const { important } = this.state;
 
 		if (done) {
 			classNameSpanLabels += " done";
 		}
-
-		const styleSpan = {
-			color: important ? "red" : "black",
-		};
+		if (important) {
+			classNameSpanLabels += " important";
+		}
 
 		return (
 			<span className={classNameSpanLabels}>
-				<span
-					style={styleSpan}
-					className="todo-list-item-label"
-					onClick={this.onLabelClick}
-				>
+				<span className="todo-list-item-label" onClick={this.onLabelClick}>
 					{label}
 				</span>
 
 				<div className="gap-2 d-flex">
-					<button type="button" className="btn btn-outline-success btn-sm">
+					<button
+						type="button"
+						className="btn btn-outline-success btn-sm"
+						onClick={this.onBtnMarkImportantClick}
+					>
 						<i className="fa fa-exclamation" />
 					</button>
 
