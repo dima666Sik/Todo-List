e06e7ac9a5f6202d266cd7218eb8124cf63bc35d
dima666Sik <leniviy.demon.1@gmail.com>
2023-02-09 01:18:18 +0200

    add feature add task, fix problem.

--- client/src/components/itemAddTask/ItemAddTask.js
+++ client/src/components/itemAddTask/ItemAddTask.js
@@ -2,25 +2,33 @@ import { Component } from "react";
 import "./ItemAddTask.css";
 
 export default class ItemAddTask extends Component {
-	render() {
-		const { onAdded } = this.props;
+	state = {
+		label: "",
+	};
 
+	onLabelChange = (event) => {
+		this.setState({ label: event.target.value });
+	};
+
+	onSubmit = (event) => {
+		event.preventDefault();
+		this.props.onAdded(this.state.label);
+	};
+
+	render() {
 		return (
-			<div className="bottom-panel d-flex">
+			<form className="bottom-panel d-flex" onSubmit={this.onSubmit}>
 				<input
 					type="text"
 					className="form-control new-todo-label"
 					placeholder="What the task you want to add?"
+					onChange={this.onLabelChange}
 				/>
 
-				<button
-					type="submit"
-					className="btn btn-outline-secondary btn-light"
-					onClick={() => onAdded("hi")}
-				>
+				<button type="submit" className="btn btn-outline-secondary btn-light">
 					Add
 				</button>
-			</div>
+			</form>
 		);
 	}
 }
